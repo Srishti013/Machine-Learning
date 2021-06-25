e1b3a7caf2d0a265c80ac8bb454a2c02e0387da4
Srishti Ahlawat <67790510+Srishti013@users.noreply.github.com>
2021-06-25 22:20:19 +0530

    Update compare_growth_rate.py

--- time_series/compare_growth_rate.py
+++ time_series/compare_growth_rate.py
@@ -16,4 +16,39 @@ plt.show()
 
 #################################################################
 
+# Import stock prices and index here
+stocks = pd.read_csv('nyse.csv',parse_dates=['date'],index_col='date')
+dow_jones = pd.read_csv('dow_jones.csv',parse_dates=['date'],index_col='date')
+
+# Concatenate data and inspect result here
+data = pd.concat([stocks,dow_jones],axis=1)
+print(data.info())
+
+# Normalize and plot your data here
+normalize= data.div(data.iloc[0]).mul(100).plot()
+plt.show()
+
+#################################################################
+
+# Create tickers
+tickers = ['MSFT','AAPL']
+
+# Import stock data here
+stocks = pd.read_csv('msft_aapl.csv',parse_dates=['date'],index_col='date')
+
+# Import index here
+sp500 = pd.read_csv('sp500.csv',parse_dates=['date'],index_col='date')
+
+# Concatenate stocks and index here
+data = pd.concat([stocks,sp500],axis=1).dropna()
+
+# Normalize data
+normalized = data.div(data.iloc[0]).mul(100)
+
+# Subtract the normalized index from the normalized stock prices, and plot the result
+normalized[tickers].sub(normalized['SP500'],axis=0).plot()
+plt.show()
+
+
+#################################################################
 
